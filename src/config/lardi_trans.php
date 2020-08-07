e62a91b5621b6f58c09c7d95a2dc17a818b6926f
Prokhorenko Vladyslav <38713205+prokhorenko-vladyslav@users.noreply.github.com>
2020-08-07 10:37:35 +0300

    Updated package config

--- src/config/lardi_trans.php
+++ src/config/lardi_trans.php
@@ -1,9 +1,37 @@
 <?php
     return [
         'api_token' => '',
+        'api_url' => 'https://api.lardi-trans.com/v2/references/',
         'models' => [
-            'country' => '',
-            'region' => '',
-            'city' => '',
+            /**
+             * Counries
+             */
+            'country' => [
+                'model' => \Illuminate\Database\Eloquent\Model::class,
+                'name_field' => 'name',
+                'sign_field' => 'sign',
+                'lardi_trans_id_field' => 'lardi_trans_id'
+            ],
+            /**
+             * Areas
+             */
+            'region' => [
+                'model' => \Illuminate\Database\Eloquent\Model::class,
+                'name_field' => 'name',
+                'lardi_trans_id_field' => 'lardi_trans_id',
+                'country_relation_method' => 'country'
+            ],
+            /**
+             * Cities
+             */
+            'city' => [
+                'model' => \Illuminate\Database\Eloquent\Model::class,
+                'name_field' => 'name',
+                'lardi_trans_id_field' => 'lardi_trans_id',
+                'latitude_field' => 'latitude',
+                'longitude_field' => 'longitude',
+                'country_relation_method' => 'country',
+                'region_relation_method' => 'region'
+            ],
         ]
     ];
